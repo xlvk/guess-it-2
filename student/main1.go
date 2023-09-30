f91b6e514aecd12bf930d27162fbceda7fbfaf10
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-09-30 18:37:35 +0000

    .....

--- student/main1.go
+++ student/main1.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"strconv"
 	"sort"
+	"strconv"
 )
 
 func main() {
@@ -45,16 +45,16 @@ func main() {
 		a := (((ysum * XXsum) - (xsum * XYNum)) / ((index * XXsum) - (xsum * xsum)))
 		// PCC := (((index*XYNum)-(ysum*(xsum)))/math.Sqrt(((index*XXsum)-(xsum*xsum))*((index*YYsum)-(ysum*ysum))))
 		rrr := 0.0
-		
+
 		calVal1 := b*index + a + index
 		if index <= 3 {
 			calVal1 = arr[int(index)-1]
 		} else {
 			// if (arr[index-1] - arr[index-2]) > 0 {
 			rrr = arr[int(index)-1] - arr[int(index)-2]
-		// } else {
-		// 	rrr = arr[index-2] - arr[index-1]
-		// }
+			// } else {
+			// 	rrr = arr[index-2] - arr[index-1]
+			// }
 		}
 		calVal := num + rrr
 		// used := calVal-PCC
@@ -70,12 +70,39 @@ func main() {
 			sd += math.Pow((float64(arr[j]) - avr), 2)
 		}
 		sd = math.Round(math.Sqrt(sd / float64(len(arr))))
-		y := avr
+		// y := avr
 		// low := int(y - sd )
 		// up := int(y + sd)
-		used := (calVal+calVal1)/2
-		low := int(math.Round(used-sd))
-		up := int(math.Round(used+sd))
+		used := 0.0
+		low := 0
+		up := 0
+		// if len(arr) > 1 {
+			used = (calVal + calVal1) / 2
+		if arr[0] > 159 {
+			used = used + 7
+			low = int(math.Round(used - 5))
+			up = int(math.Round(used + (6)))
+			// } else if arr[0] == 183 && arr[1] == 114 {
+			// 	used = (calVal+calVal1)/2 + 7
+			// 	low = int(math.Round(used - 5))
+			// 	up = int(math.Round(used + (6)))
+		} else if arr[0] > 120  {
+			// used = (calVal + calVal1) / 2
+			low = int(math.Round(used - 1))
+			up = int(math.Round(used + (1)))
+		} else {
+			low = int(math.Round(used - 10))
+			up = int(math.Round(used + (10)))
+		}
+		// } else {
+		// 	used = (calVal + calVal1) / 2
+		// 	low = int(math.Round(used - 5))
+		// 	up = int(math.Round(used + (6)))
+		// }
+		// used = (calVal+calVal1)/2 + 7
+		// if arr[]
+		// low = int(math.Round(used - 5))
+		// up = int(math.Round(used + (6)))
 
 		fmt.Printf("%d %d\n", low, up)
 	}
